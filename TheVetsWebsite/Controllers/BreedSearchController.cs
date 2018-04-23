31f6accce832cd7c74357255357738be4992d43a
Jake Robson <boirobson@gmail.com>
2018-04-23 10:28:38 +0100

    Added BreedSearchController and link on home page.  Breed Search index not working with Animal Model.  Pushing so that available at work.

---
+++ TheVetsWebsite/Controllers/BreedSearchController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TheVetsWebsite.Models;
+
+namespace TheVetsWebsite.Controllers
+{
+    public class BreedSearchController : Controller
+    {
+        // GET: BreedSearch
+        public ViewResult index()
+        {
+            var animals = new List<Animal>
+            {
+                new Animal {breed = "Boston Terrier"},
+                new Animal {breed = "Labrador"},
+                new Animal {breed = "Jack Russel"}
+            };
+            return View(animals);
+        }
+    }
+}
---
+++ TheVetsWebsite/Models/Animal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace TheVetsWebsite.Models
+{
+    public class Animal
+    {
+        public int id { get; set; }
+        public string animaltype { get; set; }
+        [DisplayName("Breed")]
+        public string breed { get; set; }
+        [DisplayName("Min Size (ft)")]
+        public float minsize { get; set; }
+        [DisplayName("Max Size (ft)")]
+        public float maxize { get; set; }
+        [DisplayName("Min Weight (lb)")]
+        public float minweight { get; set; }
+        [DisplayName("Max Weight (lb)")]
+        public float maxweight { get; set; }
+        [DisplayName("Energy Level")]
+        public int energylevel { get; set; }
+        [DisplayName("Temperament")]
+        public string temperament { get; set; }
+        [DisplayName("Trainability")]
+        public int trainability { get; set; }
+        [DisplayName("Life Span")]
+        public int lifespan { get; set; }
+        [DisplayName("Good With Kids?")]
+        public bool goodwithkids { get; set; }
+        [DisplayName("Good With Other Animals?")]
+        public bool goodwithotheranimals { get; set; }
+    }
+}
